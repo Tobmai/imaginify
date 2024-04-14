e2df0ff8e1d8e3f08668f2be206530033c1c5316
Tobmai <nazarii.iaremii@outlook.com>
2024-04-14 22:50:35 +0200

    Added restore and remove ability

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -19,7 +19,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { useState, useTransition } from "react";
+import { useEffect, useState, useTransition } from "react";
 import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils";
 import { Button } from "../ui/button";
 import { updateCredits } from "@/lib/actions/user.actions";
@@ -190,6 +190,12 @@ const TransformationForm = ({
       await updateCredits(userId, creditFee);
     });
   };
+
+  useEffect(() => {
+    if (image && (type === "restore" || type === "removeBackground")) {
+      setNewTransformation(transformationType.config);
+    }
+  }, [image, transformationType.config, type]);
 
   return (
     <Form {...form}>
